072aabc12614865da72ebc7f565ae8ea300356a8
Justin Dennison <justin1dennison@gmail.com>
2018-12-13 10:42:20 -0500

    set up convertd cli application

---
+++ convertd/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name = 'convertd',
+    version = '0.1.0',
+    packages = ['convertd'],
+    entry_points = {
+        'console_scripts': [
+            'convertd = convertd.__main__:main'     
+        ] 
+    },
+    required=[]
+)
