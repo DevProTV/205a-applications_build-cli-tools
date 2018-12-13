8c2e0346677c3496f7d2bd92330212891026035c
Justin Dennison <justin1dennison@gmail.com>
2018-12-13 10:21:32 -0500

    added error handling for incorrect urls

--- isitup/isitup/__main__.py
+++ isitup/isitup/__main__.py
@@ -1,16 +1,26 @@
+from textwrap import dedent
+from .functions import get_args, normalize_url
+
 import requests
 from colorama import Fore
-from .functions import get_args, normalize_url
+
+
 
 def main():
     args = get_args()
     url = normalize_url(args.url)
-    res = requests.get(url)
-    if res.status_code != 200:
-        print(f'{Fore.RED}The website {args.url} is down...')
-    else:
-        print(f'{Fore.GREEN}The website {args.url} is up!')
-
-
+    try:
+        res = requests.get(url)
+        if res.status_code != 200:
+            print(f'{Fore.RED}The website {args.url} is down...')
+        else:
+            print(f'{Fore.GREEN}The website {args.url} is up!')
+    except requests.exceptions.ConnectionError as e:
+        message = '\n'.join(
+                (f'{Fore.YELLOW}Could not connect to {args.url}.',
+                'Check your url and try again.')
+            )
+        print(message)
+    
 if __name__ == '__main__':
     main()
