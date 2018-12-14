f96e240ffa769492d291634909894ef913e9673e
Justin Dennison <justin1dennison@gmail.com>
2018-12-14 08:09:32 -0500

    first version of imgsizer

--- imgsizer/imgsizer/__main__.py
+++ imgsizer/imgsizer/__main__.py
@@ -1,8 +1,25 @@
+from PIL import Image
+import click
 
 
-def main():
-    pass
+@click.command()
+@click.argument("src", type=click.Path(exists=True))
+@click.option("--width", default=600, type=int, help="width of the image")
+@click.option("--height", default=400, type=int, help="height of the image")
+@click.option("--image-type", default="png", type=str, help="format of the saved image")
+def main(src, width, height, image_type):
+    """
+        CLI to resize images.
+    """
+    click.echo("Started resizing....")
+    im = Image.open(src)
+    size = (width, height)
+    original, ext = src.split(".")
+    filename = f"{original}.{image_type}"
+    im.thumbnail(size)
+    im.save(filename)
+    click.echo("Finished resizing...")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- imgsizer/setup.py
+++ imgsizer/setup.py
@@ -1,13 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'imgsizer',
-    version = '0.1.0',
-    packages = ['imgsizer'],
-    entry_points = {
-        'console_scripts': [
-            'imsizer = imgsize.__main__:main'     
-        ]
-    },
-    packages = find_packages()
+    name="imgsizer",
+    version="0.1.0",
+    packages=["imgsizer"],
+    entry_points={"console_scripts": ["imgsizer = imgsizer.__main__:main"]},
 )
